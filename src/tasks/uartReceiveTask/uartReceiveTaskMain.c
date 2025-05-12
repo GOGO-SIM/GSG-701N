23ef720d3e64d05f5470920fae18057f3fc99a90
jeongyunnim <jeongyun7521@gmail.com>
2025-05-12 14:15:16 +0900

    docs: UART 수신 태스크 수도코드 작성 완료

--- src/tasks/uartReceiveTask/uartReceiveTaskMain.c
+++ src/tasks/uartReceiveTask/uartReceiveTaskMain.c
@@ -26,13 +26,10 @@ void uartReceiveTaskMain()
 	 * 2. gsmpMsgHeader 변수에 memcpy한다.
 	 * 3. CRC 검사를 진행한다.
 	 * 3.1. CRC 검사가 일치하는지 검사한다.
-	 * 3.1.1. 일치하지 않는 경우 gUartFailCount += 1
+	 * 3.1.1. CRC가 일치하지 않거나, 응답 코드가 OK가 아닌 경우 gUartFailCount += 1
 	 * 3.1.2. gUartFailCount > 10 이라면 폭파 진행한다.
-	 * 4. messageStatus == OK를 검사한다.
-	 * 4.1. messageStatus != OK인 경우 gUartFailCount += 1
-	 * 4.1.2. gUartFailCount > 10 이라면 폭파 진행한다.
-	 * 5. gUartFailCount = 0
-	 * 6. 종료
+	 * 4. gUartFailCount = 0
+	 * 5. 종료
 	 * */
 	int isCRCSuccess;
 	int messageStatus;
@@ -43,35 +40,31 @@ void uartReceiveTaskMain()
 	messageStatus = TRUE;
 
 	// 2. gsmpMsgHeader 변수에 memcpy한다.
-	// memcpy(&gsmpMsgHeader, gUartBuffer, sizeof(gsmpMsgHeader));
-	getUartMsg(gsmpMsgHeader);
+	memcpy(&gsmpMsgHeader, gUartBuffer, sizeof(gsmpMsgHeader));
+	// getUartMsg(gsmpMsgHeader); -> TODO: 초기화 시 UART RX interrupt를 설정해야 한다.
 
 	// 3. CRC 검사를 진행한다.
 	isCRCSuccess = checkCRC(gsmpMsgHeader);
 	// 3.1. CRC 검사가 일치하는지 검사한다.
 	// 3.1.1. 일치하지 않는 경우 gUartFailCount += 1
-	if(isCRCSuccess == FALSE)
+
+	/**
+	 * [수신한 message 에러 검사]:
+	 * CRC를 실패한 경우: GCU에서 계산한 결과와 불일치
+	 * message 상태가 OK가 아닌 경우: 수신 측(ACB)의 CRC 오류 또는 네트워크 오류
+	 */
+	if (isCRCSuccess == FALSE || messageStatus != OK)
 	{
 		gUartFailCount += 1;
+		// 3.1.2. gUartFailCount > 10 이라면 폭파 진행한다.
+		if (gUartFailCount > 10)
+		{
+			explode();
+		}
 	}
-	else
-	{
-		// 4. messageStatus == OK를 검사한다.
-		// 4.1. messageStatus != OK인 경우 gUartFailCount += 1
-		if(messageStatus == FALSE)
-			{
-				gUartFailCount += 1;
-			}
-	}
-	// 3.1.2. gUartFailCount > 10 이라면 폭파 진행한다.
-	// 4.1.2. gUartFailCount > 10 이라면 폭파 진행한다.
-	if(gUartFailCount > 10)
-	{
-		explode();
-	}
-	// 5. gUartFailCount = 0
+	// 4. gUartFailCount = 0
 	gUartFailCount = 0;
-	// 6. 종료
+	// 5. 종료
 	return;
 }
 
@@ -79,5 +72,5 @@ void uartReceiveTaskMain()
 void explode()
 {
 	printf("explode\n");
-	return;
+	return ;
 }
