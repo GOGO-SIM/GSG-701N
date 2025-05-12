0153c45ea17e02b3f2a63b4c5584a8865432325e
Sepose <well@daum.net>
2025-05-12 09:23:30 +0900

    feat: PBIT 검사 태스크 수도코드 작성 #4

--- src/tasks/pbitTask/pbitTaskMain.c
+++ src/tasks/pbitTask/pbitTaskMain.c
@@ -1,85 +1,155 @@
-static int sPbitResultFlag;
+static int sPbitFail;
+//static ??? imuData;
+//static ??? seekerData;
+
+static void imuRxCallback(){
+	//imu 데이터 받아오기
+}
+
+static void seekerRxCallback(){
+	//seeker 데이터 받아오기
+}
+
+static void timerOvfCallback(){
+	sPbitFail |= setErrorFlag;
+}
 
 static int checkPower()
 {
-	sPbitResultFlag |= POWER_ERROR;
-}
-
-static int checkClock()
-{
-	sPbitResultFlag |= CLOCK_ERROR;
-}
-
-static void inspectBoard()
-{
 	/**
-	 * 1. 전압 검사
-	 * 2. 클럭 검사
+	 * [checkPower]
+	 *
+	 * 1. XADC 초기화
+	 * 2. XADC 채널 선택
+	 * 3. XADC 변환 대기
+	 * 4. 값 읽기
+	 * 5. 임계값 비교
+	 *
 	 */
 	/*****************************[임시 구현]*****************************/
-	// 1. 전압 검사
-	if (checkPower())
+
+	//1. XADC 초기화
+
+	//2. XADC 채널 선택
+
+	//3. XADC 변환 대기
+	while (!xADC_IsConversionDone());
+
+	//4. 값 읽기
+	int rawValue = xADC_GetData();
+    double measuredVoltage = xADC_ConvertToVoltage(rawValue);
+
+	//5.임계값 비교
+	if (measuredVoltage < 4.5)
 	{
-		// 2. 클럭 검사
-		checkClock();
+		sPbitFail |= setErrorFlag;
 	}
-	return ;
 }
 
 static void checkUart()
 {
-	// 1. physical loopback 검사
-	// 2. echoing 검사
-}
-
-static void checkTcp()
-{
-	// 1. L1 검사
-	// 2. L2 검사
-	// 3. L3 검사
-	// 4. L4 검사
-	// 5. echoing 검사
-}
-
-static void inspectNetwork()
-{
 	/**
-	 * 1. UART를 검사한다. (physical loopback, echoing)
-	 * 2. TCP/IP와 UDP/IP를 검사한다. (L1, L2, L3, APP)
+	 * [checkPower]
+	 *
+	 * 1. UART 초기화
+	 * 2. 송신 버퍼 준비
+	 * 3. 데이터 송신
+	 * 4. 데이터 수신 대기
+	 * 5. 수신된 데이터 읽기
+	 * 6. 송신 데이터와 수신 데이터 비교
+	 *
 	 */
-
 	/*****************************[임시 구현]*****************************/
-	checkUart();
-	checkTcp();
 
+	// 1. UART 초기화
+	UartInit();
+
+	// 2. 송신 버퍼 준비
+	char txData = 'A';
+
+	// 3. 데이터 송신
+	UartSend(txData);
+
+	// 4. 데이터 수신 대기
+	while (!UartIsDataAvailable());
+
+	// 5. 수신된 데이터 읽기
+	char rxData = UartReceive();
+
+	// 6. 송신 데이터와 수신 데이터 비교
+	if (rxData != txData)
+	{
+	   sPbitFail |= setErrorFlag;
+	}
+}
+
+static void checkNetwork(){
+
+	/**
+	 * [checkNetwork]
+	 *
+	 * 1. Timer Start
+	 * 2. 데이터 수신 대기
+	 * 3. Imu / Seeker 데이터 도착시 인터럽트 발생 후 수신
+	 * 4. 데이터 수신 후 비교
+	 * 5. 타이머 종료 및 초기화
+	 *
+	 */
+	/*****************************[임시 구현]*****************************/
+
+	//1.Timer Start
+	//타이머 초기화 및 시작
+
+	//2.데이터 수신 대기
+
+	while(); //둘다 도착하거나, 에러 발생시 까지 대기 설계하기 나름
+	//timerOvfCallback()발생시 에러 발생 flag set
+
+	//3.Imu / Seeker 데이터 도착시 인터럽트 발생 후 수신
+	//imuRxCallback() 과 seekerRxCallback()에 정의
+
+	//4.데이터 수신 후 비교
+	//-> 받아야 할 값이 왔는지 확인
+
+	//5.타이머 종료 및 초기화
 }
 
 void pbitTaskMain( void *pvParameters )
 {
 	/**
 	 * [PBIT 검사 수행]
-	 * 1. 보드를 검사한다.
-	 * 2. 연결된 기기의 네트워크를 검사한다.
-	 * 3. 텔레메트리 데이터 생성
-	 * 4. CBIT과 stanby, telemetry task를 깨운다.
+	 *
+	 * 1. 전압을 체크한다.
+	 * 2. Uart 상태를 체크한다.
+	 * 3. 연결된 기기의 네트워크를 검사한다.
+	 * 4. 다음 task를 깨운다.
 	 * 5. PBIT task를 삭제한다.
+	 *
 	 */
 
 	/*****************************[임시 구현]*****************************/
-	// 1. 보드를 검사한다.
-	inspectBoard();
-	if (sPbitResultFlag == false)
+
+	// 1. 전압을 체크한다.
+	checkPower();
+
+	if (!sPbitFail)
 	{
-		// 2. 연결된 기기의 네트워크를 검사한다.
-		inspectNetwork();
+	// 2. Uart 상태를 체크한다
+		checkUart();
+
+	// 3. 연결된 기기의 네트워크를 검사한다.
+		checkNetwork();
 	}
 
-	// 3. 텔레메트리 데이터 생성
-	// pushPbitDataIntoQueue(sPbitResultFlag)
-
-	// 4. CBIT과 stanby task를 깨운다.
-	// xTaskResume(xHandleCbit);
-	// xTaskResume(xHandleStanby);
+	// 4. 다음task를 깨운다.
+	if (!sPbitFail)
+	{
+	// xTaskResume(콜드런치 점화 대기 상태 태스크);
+	}
+	else
+	{
+	// xTaskResume(PBIT 실패 제어 차단 상태 태스크);
+	}
 
 	// 5. pbit task를 삭제한다.
 	// xTaskDelete(xHandlePbit);
