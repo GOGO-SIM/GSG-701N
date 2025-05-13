1b3e26b569f4da6b5cbe3341e2005f282f7c4aae
Soyoung Lee <82654401+jordan-comlinee@users.noreply.github.com>
2025-05-13 17:28:49 +0900

    fix: pseudo 코드 수정 및 전역 변수 분리 todo 추가 #7

--- src/tasks/uartSendTask/uartSendTaskMain.c
+++ src/tasks/uartSendTask/uartSendTaskMain.c
@@ -1,15 +1,14 @@
 #include "includes/gsgTypes.h"
 #include "includes/taskMain.h"
 
-typedef struct _sMessageHeader
-{
+typedef struct _sMessageHeader {
 	uint8_t startflag;
 	uint8_t msgId;
 	uint8_t srcId;
 	uint8_t destId;
 	uint8_t msgStat;
 	uint16_t msgLen;
-}sMessageHeader;
+} sMessageHeader;
 
 static int recvFlag;
 extern int gRecvMissCount;
@@ -18,19 +17,16 @@ void explode();
 void setCrc(sMessageHeader* acbMsg, sMessageHeader* fullMsgBuffer);
 void sendData(uint32_t buffer);
 
-void uartSendTaskMain( void *pvParameters )
-{
+void uartSendTaskMain(void *pvParameters) {
 	sMessageHeader acbMsg;
 	recvMissCount = 0;
 	uint32_t buffer;
 
 	// 1. gIsUartReceive가 True인지 확인한다.
-	if(recvFlag == False)
-	{
+	if (recvFlag == False) {
 		// 1.1. gIsUartReceive == False인 경우 recvMissCount +=1;
 		// 1.1.1. recvMissCount >= 10 이라면 폭파한다.
-		if(++gRecvMissCount >= 10)
-		{
+		if (++gRecvMissCount >= 10) {
 			explode();
 		}
 	}
@@ -44,14 +40,12 @@ void uartSendTaskMain( void *pvParameters )
 	sendData(buffer);
 }
 
-
-void setCrc(sMessageHeader acbMsg, sMessageHeader* fullMsgBuffer)
-{
+void setCrc(sMessageHeader acbMsg, sMessageHeader* fullMsgBuffer) {
 	// CRC는 마지막 2바이트에 저장될 예정
 	uint16_t totalLength = sizeof(sMessageHeader) + acbMsg.msgLen;
 	// CRC 계산 (CRC 자체는 포함하지 않음)
 	uint16_t crc = crc16_xmodem(fullMsgBuffer, totalLength);
-	fullMsgBuffer[totalLength] = crc& 0xFF;
-	fullMsgBuffer[totalLength + 1] = (crc>>8) & 0xFF;
+	fullMsgBuffer[totalLength] = crc & 0xFF;
+	fullMsgBuffer[totalLength + 1] = (crc >> 8) & 0xFF;
 
 }
