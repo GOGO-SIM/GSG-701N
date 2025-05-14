4e2e19b7a4085a1d66522a4648a0c1c8bbf34c09
Soyoung Lee <82654401+jordan-comlinee@users.noreply.github.com>
2025-05-14 11:25:55 +0900

    fix: uartSendTask  호출로 수정 #6

--- src/tasks/standbyIgnitiontask/standbyIgnitionTaskMain.c
+++ src/tasks/standbyIgnitiontask/standbyIgnitionTaskMain.c
@@ -12,6 +12,6 @@ void pbitFailTaskMain( void *pvParameters )
 	// 1. 전역 커맨드 변수의 payload의 current Status를 STANDBY로 설정
 	gGcuStatus = STANDBY;
 	// 2. ACB CMD 송신 태스크 깨움
-	xTaskNotifyGive(uartReceiveTask);
+	xTaskNotifyGive(uartSendTask);
 
 }
