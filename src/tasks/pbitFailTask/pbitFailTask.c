4dd2cc21cac2cf5912f619f4004100ba004a1464
Soyoung Lee <82654401+jordan-comlinee@users.noreply.github.com>
2025-05-14 11:30:12 +0900

    fix: uartSendTask 송신 태스크 호출 수정

--- src/tasks/pbitFailTask/pbitFailTask.c
+++ src/tasks/pbitFailTask/pbitFailTask.c
@@ -12,6 +12,6 @@ void pbitFailTaskMain( void *pvParameters )
 	// 1. 전역 커맨드 변수의 payload의 Current Status를 PBIT_FAIL로 설정
 	gGcuStatus = PBIT_FAIL;
 	// 2. ACB CMD 송신 태스크 깨움
-	xTaskNotifyGive(uartReceiveTask);
+	xTaskNotifyGive(uartSendTask);
 
 }
