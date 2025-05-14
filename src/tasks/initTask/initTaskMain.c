f0f8ee4b8ac211742ce0e8b6741570b15d697957
jeongyunnim <jeongyun7521@gmail.com>
2025-05-14 11:09:07 +0900

    feat: GCU init task 및 모든 태스크 생성 부 수도코드 작성

---
+++ src/tasks/initTask/initTaskMain.c
@@ -0,0 +1,15 @@
+
+void initUdpServer()
+{
+	// lwip의 udp 서버 초기화 방식 3가지 중 무엇을 선택할 지 결정하여 구현
+}
+
+void initTaskMain( void *pvParameters )
+{
+	/**
+	 * 1. UDP server open
+	 * 2. delete task
+	 */
+	initUdpServer();
+	vTaskDelete(xInitTaskHandle);
+}
