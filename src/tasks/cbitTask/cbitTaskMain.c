153c9a32c8f738b042dad8b726a84bb514893443
sepose <87268767+sepose@users.noreply.github.com>
2025-05-16 20:02:11 +0900

    feat: CBIT 코드 작성

--- src/tasks/cbitTask/cbitTaskMain.c
+++ src/tasks/cbitTask/cbitTaskMain.c
@@ -1,85 +1,218 @@
-#include <stdio.h>
+// FreeRTOS 관련 include
+
 #include "FreeRTOS.h"
-#include "gsgTypes.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+
+// 전역변수 include
+
 #include "global.h"
 
-static int sPassCbitFlag = TRUE; // 통과 == TRUE, 실패 == FALSE
+// Xilinx 관련 include
 
-int xADC_IsConversionDone();
+#include "xsysmon.h"
+#include "xil_printf.h"
+#include "xil_io.h"
+#include "xuartps.h"
+#include "xparameters.h"
+
+// Lwip 관련 include
+
+#include "lwip/tcpip.h"
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "lwip/api.h"
+#include "netif/xadapter.h"
+#include "lwip/netbuf.h"
+#include "lwip/ip_addr.h"
+#include "netif/xemacpsif.h"
+#include "platform_config.h"
+
+XSysMon sysMonInst;
+XSysMon_Config *configPtr;
+
+//-----------------------------( 상태 체크 용도 )----------------------------------------
+
+static u32 sEccStatus; // 에러 발생시 set Error Correcting Code
+static u32 sAxiError;
+static u32 sCacheError;
+
+static u32 sUartStatus;
+static u32 sTxStatusReg;
+static u32 sRxStatusReg;
+static u32 sPassCbitFlag = TRUE; // 통과 == TRUE, 실패 == FALSE
+
+//----------------------------( 전압 및 온도 측정용  )-------------------------------------
+
+static u16 sRawVccInt;
+static u16 sRawVccAux;
+static u16 sRawVccRam;
+static u16 sRawTemperture;
+
+double gVoltageInt;
+double gVoltageBram;
+double gVoltageAux;
+double gCelcius;
+
+XUartPs_Config uartConfig; // (To do : 나중에 지워주세요. -> Uart init은 외부에서 완료됨. )
+
+// -------------------( 나중에 Task 시작 전 한번 init 해주어야 한다.)-------------------------
+
+void initXsysMon()
+{
+    configPtr = XSysMon_LookupConfig(XPAR_SYSMON_0_DEVICE_ID);
+    XSysMon_CfgInitialize(&sysMonInst, configPtr, configPtr->BaseAddress);
+    XSysMon_SetSequencerMode(&sysMonInst, XSM_SEQ_MODE_CONTINPASS);
+}
+
+//void debug()
+//{
+//	        int int_vccint = (int)gVoltageInt;
+//	        int frac_vccint = (int)((gVoltageInt - int_vccint) * 1000);
+//	        int int_vccaux = (int)gVoltageBram;
+//	        int frac_vccaux = (int)((gVoltageAux - int_vccaux) * 1000);
+//	        int int_vccbram = (int)gVoltageAux;
+//	        int frac_vccbram = (int)((gVoltageBram - int_vccbram) * 1000);
+//	        int int_temp = (int)gCelcius;
+//	        int frac_temp = (int)((gCelcius - int_temp) * 1000);
+//
+//	        xil_printf("VCCINT: %d.%03d V, VCCAUX: %d.%03d V, VCCBRAM: %d.%03d V, TEMP: %d.%03d'C ErrCNT: %d\r\n",
+//	                           int_vccint, frac_vccint,
+//	                           int_vccaux, frac_vccaux,
+//	                           int_vccbram, frac_vccbram,
+//	                           int_temp, frac_temp,sErrorCount);
+//}
+
+// -------------------------------------------------------------------------------
 
 static void checkPower()
 {
-	/*
-	 * 1. XADC 변환 대기
-	 * 2. 값 읽기
-	 * 3. 임계값 비교
-	 * 초기화 관련은 이미 PBIT에서 진행하였음으로 더 하지 않음
-	 */
-	/*****************************[임시 구현]*****************************/
-	//1. XADC 변환 대기
-	while (!xADC_IsConversionDone());
+	// 각 측정 데이터 Raw 값으로 받아오기
 
-	//2. 값 읽기
-	int rawValue = xADC_GetData();
-    double measuredVoltage = xADC_ConvertToVoltage(rawValue);
+	sRawVccInt = XSysMon_GetAdcData(&sysMonInst, XSM_CH_VCCINT);
+	sRawVccAux = XSysMon_GetAdcData(&sysMonInst, XSM_CH_VCCAUX);
+	sRawVccRam = XSysMon_GetAdcData(&sysMonInst, XSM_CH_VBRAM);
+	sRawTemperture = XSysMon_GetAdcData(&sysMonInst, XSM_CH_TEMP);
 
-	//3.임계값 비교
-	if (measuredVoltage < gVoltage1)
+	// Raw 데이터를 실제 전압,온도로 변환
+
+	gVoltageInt = XSysMon_RawToVoltage(sRawVccInt);
+	gVoltageBram = XSysMon_RawToVoltage(sRawVccAux);
+	gVoltageAux = XSysMon_RawToVoltage(sRawVccRam);
+	gCelcius = XSysMon_RawToVoltage(sRawTemperture);
+
+	// debug();
+
+	// 비정상 전압 혹은 온도 감지시 sPassCbitFlag False로 전환
+
+	if ( gVoltageInt < 0.9 || gVoltageInt > 1.05 )
 	{
-		sPassCbitFlag == FALSE;
+		sPassCbitFlag = FALSE;
 	}
-}
-
-int checkAllRegister()
-{
-	return TRUE;
-}
-
-static void checkRegister()
-{
-	/*
-	 * 1. 모든레지스터 검사
-	 */
-	/*****************************[임시 구현]*****************************/
-
-	// 1. 모든레지스터 검사
-	if (checkAllRegister() == TRUE)
+	if ( gVoltageBram < 0.9 || gVoltageBram > 1.05 )
 	{
-		// 레지스터가 이상할때 set 되는지, 정상일때 set 되는지 아직 판단 불가
+		sPassCbitFlag = FALSE;
+	}
+	if ( gVoltageAux < 1.7 || gVoltageAux > 1.9 )
+	{
+		sPassCbitFlag = FALSE;
+	}
+	if ( gCelcius > 95 )
+	{
 		sPassCbitFlag = FALSE;
 	}
 }
 
-void cbitTaskMain( void *pvParameters )
+static void checkUart()
 {
-	/*
-	 * 1. 전압 검사
-	 * 2. 레지스터 검사
-	 * 3. 플래그 검사 및 폭발
-	 */
-	/*****************************[임시 구현]*****************************/
+	sUartStatus = XUartPs_ReadReg(uartConfig->BaseAddress,XUARTPS_ISR_OFFSET);
+
+	if (sUartStatus & XUARTPS_IXR_PARITY)
+	{
+		sPassCbitFlag = FALSE; // Uart 패리티 에러 Set
+	}
+	if (sUartStatus & XUARTPS_IXR_FRAMING)
+	{
+		sPassCbitFlag = FALSE; // Uart 프레이밍 에러 Set
+	}
+	if (sUartStatus & XUARTPS_IXR_OVER)
+	{
+		sPassCbitFlag = FALSE; // Uart 버퍼 오버런 에러 Set
+	}
+	XUartPs_WriteReg(uartConfig->BaseAddress, XUARTPS_ISR_OFFSET, sUartStatus); // Error Reset
+}
+
+static void checkEthernet()
+{
+	sTxStatusReg = XEmacPs_ReadReg(EMAC_BASEADDR, XEMACPS_TXSR_OFFSET);
+	sRxStatusReg = XEmacPs_ReadReg(EMAC_BASEADDR, XEMACPS_RXSR_OFFSET);
+
+    if (sTxStatusReg != 0) // 0이 아니라면 Error True False로 나뉘는게 아님
+    {
+    	sPassCbitFlag = FALSE;
+        XEmacPs_WriteReg(EMAC_BASEADDR, XEMACPS_TXSR_OFFSET, sTxStatusReg);
+    }
+    if ((sRxStatusReg & ~0x01) != 0) // 첫 비트만 제외하고 0인지 검사
+    {
+    	sPassCbitFlag = FALSE;
+        XEmacPs_WriteReg(EMAC_BASEADDR, XEMACPS_RXSR_OFFSET, sRxStatusReg);
+    }
+}
+
+static void checkMemory()
+{
+    sEccStatus = Xil_In32(0xF800606C);
+    sAxiError = Xil_In32(0xF8000F40);
+    sCacheError = Xil_In32(0xF8F02100);
+
+    if (sEccStatus == TRUE || sAxiError == TRUE || sCacheError == TRUE )
+    {
+    	sPassCbitFlag = FALSE;
+    }
+
+    Xil_Out32(0xF800606C, sEccStatus);
+    Xil_Out32(0xF8000F40, sAxiError);
+    Xil_Out32(0xF8F02100, sCacheError);
+}
+
+static void checkRegister()
+{
+	checkUart();
+	checkEthernet();
+	checkMemory();
+}
+
+void runCbit()
+{
+	static int sErrorCount;
 	while(1)
 	{
-		// 1. 전압 검사
+		if(sErrorCount > 5)
+		{
+			explode(); // 5번 연속 에러시 자폭
+		}
 		checkPower();
 		if ( sPassCbitFlag == TRUE )
 		{
-		// 2. 레지스터 검사
 			checkRegister();
 		}
-		// 3. 플래그 검사 및 폭발
 		if ( sPassCbitFlag == FALSE )
 		{
-			//폭발한다.
+			sErrorCount += 1;
 		}
-		vTaskResume(xTelemetryTaskHandle);
-		vTaskSuspend(xCbitTaskHandle);
+		else if ( sPassCbitFlag == TRUE )
+		{
+			sErrorCount = 0;
+		}
 	}
-
 }
 
-
-int xADC_IsConversionDone()
+void cbitTaskMain(void *param)
 {
-	return 1;
+    for(;;)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        runCbit();
+    }
 }
