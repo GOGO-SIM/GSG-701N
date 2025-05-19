13506cf748a6ed0bbb167c701abb76c3573dee8f
Soyoung Lee <82654401+jordan-comlinee@users.noreply.github.com>
2025-05-19 12:21:05 +0900

    fix: printf() 무효화

--- src/tasks/uartSendTask/uartSendTaskMain.c
+++ src/tasks/uartSendTask/uartSendTaskMain.c
@@ -4,6 +4,8 @@
 #include "crc.h"
 #include "xuartps.h"
 #include "xparameters.h"
+// blocking 무효화
+#define xil_printf(...)  do {} while(0)
 
 extern int gRecvMissCount;
 
@@ -46,9 +48,9 @@ void uartSendDataRun()
 
 	uint8_t txBuffer[bufferSizeNoCRC + crcSize]; // CRC 포함
 //	test data
-//	gControlCmd.x = 1.02;
-//	gControlCmd.y = 2.03;
-//	gControlCmd.z = 3.04;
+	gControlCmd.x = 1.02;
+	gControlCmd.y = 2.03;
+	gControlCmd.z = 3.04;
 	// 헤더 설정
 	targetMsg.header.startflag = START_FLAG;
 	targetMsg.header.msgId = ACB_MSG_ID;
@@ -63,10 +65,10 @@ void uartSendDataRun()
 	memcpy(tempBuf, (const uint8_t*) &targetMsg.header, headerSize);
 	memcpy(tempBuf + headerSize, (const uint8_t*) targetMsg.payload, sizeof(gControlCmd));
 	uint16_t crc = calcCrc(tempBuf, sizeof(tempBuf));
-
-//	xil_printf("Header = %d\r\n", headerSize);   // 예상 7
-//	xil_printf("Payload = %d\r\n", cmdSize);         // 예상 24
-//	xil_printf("TX total = %d\r\n", bufferSize);           // 예상 33
+	xil_printf("tempBuf = %d\r\n", sizeof(tempBuf));
+	xil_printf("Header = %d\r\n", headerSize);   // 예상 7
+	xil_printf("Payload = %d\r\n", cmdSize);         // 예상 24
+	xil_printf("TX total = %d\r\n", bufferSize);           // 예상 33
 
 	// 전송 버퍼 구성
 	memcpy(txBuffer, &targetMsg.header, headerSize);
@@ -74,44 +76,23 @@ void uartSendDataRun()
 	txBuffer[bufferSizeNoCRC + 0] = crc & 0xFF;        // LSB
 	txBuffer[bufferSizeNoCRC + 1] = (crc >> 8) & 0xFF; // MSB
 
-	xil_printf("TX total = %d\r\n", sizeof(txBuffer));
+	//xil_printf("TX total = %d\r\n", sizeof(txBuffer));
+	//xil_printf("x = %d y = %d z = %d\r\n", (int)(gControlCmd.x * 100), (int)(gControlCmd.y * 100), (int)(gControlCmd.z * 100));
+
+//	xil_printf("x=%.2f, y=%.2f, z=%.2f\r\n", gControlCmd.x, gControlCmd.y, gControlCmd.z);
+//	for (int i = 0; i < 33; ++i) {
+//	    xil_printf("%02X ", txBuffer[i]);
+//	}
 
 	// 전송
 	sendData(txBuffer, sizeof(txBuffer));
 }
 
-//int initUartPs()
-//{
-//	// UART 설정 정보를 담을 구조체 포인터 선언
-//    XUartPs_Config *Config;
-//    int Status;
-//
-//    // UART 디바이스 ID에 해당하는 설정 정보를 검색해서 Config에 저장
-//    // 이 정보에는 BaseAddress, BaudRate 등 하드웨어 정보 포함
-//    Config = XUartPs_LookupConfig(XPAR_PS7_UART_1_DEVICE_ID);
-//    if (Config == NULL)
-//    {
-//    	xil_printf("Config is NULL\r\n");
-//    	return XST_FAILURE;
-//    }
-//
-//    Status = XUartPs_CfgInitialize(&Uart_Ps, Config, Config->BaseAddress);
-//    if (Status != XST_SUCCESS)
-//    {
-//    	xil_printf("State is FAIL\r\n");
-//    	return XST_FAILURE;
-//   	}
-//
-//    // 인스턴스 초기화 시, default 값이19200bps이기 때문에 필요 시 명시
-//    XUartPs_SetBaudRate(&Uart_Ps, UART_BAUD);
-//    return XST_SUCCESS;
-//}
-
 
 void sendData(uint8_t* buffer, uint16_t len)
 {
     // len 바이트만큼 UART로 전송
-    XUartPs_Send(&Uart_Ps, buffer, len);
-    xil_printf("\r\n");
+    XUartPs_Send(&gUartPs, buffer, len);
+//    xil_printf("\r\n");
     //xil_printf("TX: 0x%02X %02X %02X %02X\n", buffer[0], buffer[1], buffer[2], buffer[3]);
 }
