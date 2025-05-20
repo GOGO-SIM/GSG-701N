2fc8de5355e8dc3575984ecd63474c31b72137c2
Soyoung Lee <82654401+jordan-comlinee@users.noreply.github.com>
2025-05-20 10:34:36 +0900

    feat: uart receive 1차 구현 완료
    ---
    * test data만을 수신받기 때문에 payload는 int32_t로 지정하였음
    * receive Data의 내용은 TBD이므로 추후 수정 필요 #56

--- src/tasks/uartReceiveTask/uartReceiveTaskMain.c
+++ src/tasks/uartReceiveTask/uartReceiveTaskMain.c
@@ -6,12 +6,11 @@
 
 extern int gUartFailCount;
 
-uint8_t rxBuffer[33];
+uint8_t rxBuffer[13];
 
-
+int recvUartBytes(uint8_t* buffer, int len);
 static void runUartReceive();
 void explode();
-int checkCRC(tGsmpMessageHeader examp, uint16_t size);
 
 static int isSuccess = FALSE;
 
@@ -26,35 +25,58 @@ void uartReceiveTaskMain()
     }
 }
 
+// len개의 바이트를 받을 때 까지 데이터를 수신받는다.
+int recvUartBytes(uint8_t* buffer, int len)
+{
+	int received = 0;
+	while(received < len)
+	{
+		received += XUartPs_Recv(&gUartPs, &buffer[received], len - received);
+	}
+	return TRUE;
+}
+
 static void runUartReceive()
 {
 	int received = 0;
-	while(received < 33)
+	while(received < 13)
 	{
-		received += XUartPs_Recv(&gUartPs, &rxBuffer[received], 33 - received);
+		received += XUartPs_Recv(&gUartPs, &rxBuffer[received], 13 - received);
 	}
 
-	uint16_t calculatedCrc = calcCrc(rxBuffer, 31);
-	uint16_t receivedCrc = rxBuffer[31] | (rxBuffer[32] << 8);
+	uint16_t calculatedCrc = calcCrc(rxBuffer, 11);
+	uint16_t receivedCrc = rxBuffer[11] | (rxBuffer[12] << 8);
 
 	if(calculatedCrc != receivedCrc)
 	{
 		xil_printf("CRC Error! calculated CRC : %x received CRC : %x\r\n", calculatedCrc, receivedCrc);
 		return;
 	}
+
+	xil_printf("\r\ntotal size is %d\r\n", sizeof(rxBuffer));
 	xil_printf("Received CRC is %x && Calculated CRC is %x\r\n", receivedCrc, calculatedCrc);
 	tGsmpMessageHeader header;
 	memcpy(&header, rxBuffer, sizeof(header));
 
-	tDVector3 payload;
-	memcpy(&payload, rxBuffer + sizeof(header), sizeof(payload));
+	uint8_t* ptr = rxBuffer + sizeof(tGsmpMessageHeader);  // 포인터 위치 조정
+
+	int32_t responseMsg = (int32_t)(ptr[0]) |
+	                      (int32_t)(ptr[1] << 8) |
+	                      (int32_t)(ptr[2] << 16) |
+	                      (int32_t)(ptr[3] << 24);
 	isSuccess = TRUE;
 
-	xil_printf("size = %d\r\n", sizeof(payload));
-	xil_printf("X = %f, Y = %f, Z = %f\r\n", payload.x, payload.y, payload.z);
+	//xil_printf("size = %d\r\n", sizeof(payload));
+	xil_printf(" startFlag: %x\r\n msgId: %x\r\n destId: %x\r\n srcId: %x\r\n msgLen: %d\r\n", header.startflag, header.msgId, header.destId, header.srcId, header.msgLen);
+	for(int i = 0; i < sizeof(rxBuffer); ++i)
+	{
+		xil_printf("%02x ", rxBuffer[i]);
+	}
+	xil_printf("\r\n");
 
+	xil_printf("responseMsg: %d\r\n", responseMsg);
+	xil_printf("\r\n*----------------*\r\n");
 }
-
 
 void explode()
 {
