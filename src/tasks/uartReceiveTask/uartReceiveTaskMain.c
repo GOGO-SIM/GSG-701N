cab49da1f2eb368d66ba974ff2e8a43dbf1a3a3f
Soyoung Lee <82654401+jordan-comlinee@users.noreply.github.com>
2025-05-24 21:09:38 +0900

    fix: merge from origin

--- src/tasks/uartReceiveTask/uartReceiveTaskMain.c
+++ src/tasks/uartReceiveTask/uartReceiveTaskMain.c
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
 #include "uartReceiveTaskRun.h"
-=======
-#include <stdio.h>
-#include <string.h>
-#include "global.h"
-#include "gsgTypes.h"
-#include "crc.h"
-#include "xparameters.h"
-#include "uartReceiveTaskRun.h"
-/* TODO : 테스트 데이터 예시 크기이므로 추후 리팩토링 필요 #56
- * RECEIVE_SIZE 재정의
- * exampleMsg -> recvUartMsg, 전역변수화
- * */
->>>>>>> refactor/unwrapper
 
 void uartReceiveTaskMain() 
 {
@@ -24,7 +10,3 @@ void uartReceiveTaskMain()
         runUartReceive();
     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> refactor/unwrapper
