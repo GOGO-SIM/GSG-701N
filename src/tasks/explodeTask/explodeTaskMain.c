aa2a40b7aba0fbcf1bacaa687f2152324ed74215
sepose <87268767+sepose@users.noreply.github.com>
2025-06-04 21:09:28 +0900

    fix: ACB에게 폭파 신호를 보내지 못하는 오류 수정

--- src/tasks/explodeTask/explodeTaskMain.c
+++ src/tasks/explodeTask/explodeTaskMain.c
@@ -17,8 +17,17 @@ const static int EXPLODE_TASK_PRIO = 24;
 
  void explodeTaskMain(void *param)
  {
-
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+    vTaskSuspend(xUdpReceiveTaskHandle);
+    vTaskSuspend(xNavigationTaskHandle);
+    vTaskSuspend(xGuidanceTaskHandle);
+    vTaskSuspend(xControlTaskHandle);
+    vTaskSuspend(xControlTaskHandle);
+    vTaskSuspend(xUartReceiveTaskHandle);
+    vTaskSuspend(xCbitTaskHandle);
+    vTaskSuspend(xTelemetryTaskHandle);
+
     xil_printf("RUN -- %s\r\n", pcTaskGetName(NULL));
 
     sGpioConfig = XGpioPs_LookupConfig(PS_GPIO_DEVICE_ID);
@@ -27,30 +36,25 @@ const static int EXPLODE_TASK_PRIO = 24;
     XGpioPs_SetDirectionPin(&sGpioPs, sOutPin, GPIO_OUT);
     XGpioPs_SetOutputEnablePin(&sGpioPs, sOutPin, GPIO_OUT);
 
+    XGpioPs_WritePin(&sGpioPs, sOutPin, GPIO_ON);
+
     gModeStatus = EXPLODE;
 
     //-------------------------------------------
 
-    taskENTER_CRITICAL();
+	   vTaskPrioritySet(xUartSendTaskHandle, EXPLODE_TASK_PRIO);
+	   xTaskNotifyGive(xUartSendTaskHandle);
+	   vTaskDelay(pdMS_TO_TICKS(100));
+	   vTaskSuspend(xUdpReceiveTaskHandle);
 
-    vTaskPrioritySet(xUartSendTaskHandle, EXPLODE_TASK_PRIO);
-    xTaskNotifyGive(xUartSendTaskHandle);
-    vTaskDelay(pdMS_TO_TICKS(100));
-    vTaskPrioritySet(xUartSendTaskHandle, UART_SEND_TASK_PRIO);
+	   //-------------------------------------------
 
-    taskEXIT_CRITICAL();
+	   while(1)
+	   {
+		    XGpioPs_WritePin(&sGpioPs, sOutPin, GPIO_ON);
+		    usleep(200000);
+		    XGpioPs_WritePin(&sGpioPs, sOutPin, GPIO_OFF);
+		    usleep(200000);
 
-    //-------------------------------------------
-
-    xil_printf("BOOM!\r\n");
-
-    while (1) 
-    {
-        XGpioPs_WritePin(&sGpioPs, sOutPin, GPIO_ON);
-        //xil_printf("ON!\r\n");
-        usleep(500000);
-        XGpioPs_WritePin(&sGpioPs, sOutPin, GPIO_OFF);
-        //xil_printf("OFF!\r\n");
-        usleep(500000);
-    }
+	   }
  }
